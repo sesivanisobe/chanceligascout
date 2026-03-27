8d00e39ce4aad05389b12ecba4f96eb7901e56d7
Viktor Matuna <viktormatuna@Viktor--MacBook-Air.local>
2026-03-27 23:25:38 +0100

    Fix Slavia team ID to 560

--- netlify/functions/football.js
+++ netlify/functions/football.js
@@ -1,6 +1,6 @@
 export default async (req, context) => {
   const apiKey = Netlify.env.get("FOOTBALL_API_KEY");
-  const apiUrl = `https://v3.football.api-sports.io/teams?name=Slavia Praha`;
+  const apiUrl = `https://v3.football.api-sports.io/players?team=560&season=2024&page=1`;
 
   const response = await fetch(apiUrl, {
     headers: { "x-apisports-key": apiKey },
